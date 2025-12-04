e40b4464baf90a3743e442f34949f9496f8546dd
Mente em Evolução <leandroalves.facilities@gmail.com>
2025-12-03 21:02:42 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'seu-vizinho-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest-vizinho.json',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    })))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      const fetchPromise = fetch(request).then((networkResponse) => {
+        const copy = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+        return networkResponse;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
